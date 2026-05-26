77ef85995fcc1c41cb185ab12c8ec775db5364ff
Paige T. <paige@paige.bio>
2026-05-26 02:57:43 +0000

    backup disk

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+======================================Readme=================================
+if you're using bochs on your pc , type this in Cylinders/Heads/Sectors (C=Cylnders, H=Headers, S=Sectors)
+CHS=1015/16/63
--- renderer.ts
+++ renderer.ts
@@ -18,7 +18,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     wdosboxUrl: "./dist/vendor/jsdos/wdosbox.js",
     wasmUrl: "./dist/vendor/jsdos/wdosbox.wasm",
     pathPrefix: "./dist/vendor/jsdos/",
-    backend: "dosboxX", // Add this line to force dosboxX // doesnt do anything
+    //backend: "dosboxX", // Add this line to force dosboxX // doesnt do anything
     noNetworking: false, // Must be false to enable
     ipx: [{
       type: "udp", 
@@ -33,119 +33,196 @@ window.addEventListener("DOMContentLoaded", async () => {
      // 2. Define the configuration to force DOS 7 behavior
     // The 'ver=7.10' line is how you spoof/set the reported DOS version
     const myConf = `
-[dos]
-ver=7.10
-keyboardlayout=auto
+[sdl]
+autolock=false
+
+fullscreen=false
+fulldouble=false
+fullresolution=original
+windowresolution=original
+output=surface
+sensitivity=100
+waitonerror=true
+priority=higher,normal
+mapperfile=mapper-jsdos.map
+usescancodes=true
+vsync=false
 
 [dosbox]
-language                                        =
-beep duration                                   = 0
-title                                           = Windows 98 SE
-logo text                                       =
-logo                                            =
-fastbioslogo                                    = false
-disable graphical splash                        = false
-startbanner                                     = true
-bannercolortheme                                = default
-configuration tool theme                        =
-dpi aware                                       = auto
-quit warning                                    = auto
-allow quit after warning                        = true
-working directory option                        = default
-working directory default                       =
-show advanced options                           = false
-resolve config path                             = true
-hostkey                                         = mapper
-mapper send key                                 = ctrlaltdel
-ime                                             = auto
-synchronize time                                = false
-keyboard hook                                   = false
-weitek                                          = false
-bochs debug port e9                             = false
-machine                                         = svga_s3
-captures                                        = capture
-autosave                                        =
-saveslot                                        = 1
-savefile                                        =
-video debug at startup                          = false
-saveremark                                      = true
-forceloadstate                                  = false
-compresssaveparts                               = true
-show recorded filename                          = false
-skip encoding unchanged frames                  = false
-capture chroma format                           = auto
-capture format                                  = default
-shell environment size                          = 0
-shell permanent                                 = false
-private area size                               = 32768
-a20                                             = mask
-turn off a20 gate on boot                       = true
-cbus bus clock                                  = std10
-isa bus clock                                   = std8.3
-pci bus clock                                   = std33.3
-call binary on reset                            =
-unhandled irq handler                           =
-call binary on boot                             =
-ibm rom basic                                   =
-rom bios allocation max                         = 0
-rom bios minimum size                           = 0
-irq delay ns                                    = -1
-iodelay                                         = -1
-iodelay16                                       = -1
-iodelay32                                       = -1
-acpi                                            = off
-acpi rsd ptr location                           = auto
-acpi sci irq                                    = -1
-acpi iobase                                     = 0
-acpi reserved size                              = 0
-memory file                                     =
-memsize                                         = 256
-memsizekb                                       = 0
-dos mem limit                                   = 0
-isa memory hole at 512kb                        = auto
-isa memory hole at 15mb                         = auto
-reboot delay                                    = -1
-memalias                                        = 0
-nocachedir                                      = false
-freesizecap                                     = cap
-convertdrivefat                                 = true
-convert fat free space                          = 250
-convert fat timeout                             = 4
-leading colon write protect image               = true
-locking disk image mount                        = true
-unmask keyboard on int 16 read                  = true
-int16 keyboard polling undocumented cf behavior = false
-allow port 92 reset                             = true
-enable port 92                                  = true
-enable 1st dma controller                       = true
-enable 2nd dma controller                       = true
-allow dma address decrement                     = true
-enable 128k capable 16-bit dma                  = auto
-enable dma extra page registers                 = true
-dma page registers write-only                   = false
-cascade interrupt never in service              = false
-cascade interrupt ignore in service             = auto
-enable slave pic                                = true
-enable pc nmi mask                              = true
-allow more than 640kb base memory               = false
-enable pci bus                                  = true
+machine=svga_s3
+
+language=
+captures=capture
+memsize=63
 
 [cpu]
-core=normal
+core=auto
 cputype=auto
 cycles=auto
 
-[log]
-# does nadda 
+cycleup=10
+cycledown=20
+
+[mixer]
+nosound=false
+rate=44100
+
+blocksize=1024
+prebuffer=20
+
+[render]
+# frameskip: How many frames DOSBox skips before drawing one.
+#    aspect: Do aspect correction, if your output method doesn't support scaling this can slow things down!.
+#    scaler: Scaler used to enlarge/enhance low resolution modes.
+#              If 'forced' is appended, then the scaler will be used even if the result might not be desired.
+#            Possible values: none, normal2x, normal3x, advmame2x, advmame3x, advinterp2x, advinterp3x, hq2x, hq3x, 2xsai, super2xsai, supereagle, tv2x, tv3x, rgb2x, rgb3x, scan2x, scan3x.
+
+frameskip=0
+aspect=false
+scaler=none
+
+[midi]
+#     mpu401: Type of MPU-401 to emulate.
+#             Possible values: intelligent, uart, none.
+# mididevice: Device that will receive the MIDI data from MPU-401.
+#             Possible values: default, win32, alsa, oss, coreaudio, coremidi, none.
+# midiconfig: Special configuration options for the device driver. This is usually the id of the device you want to use.
+#               See the README/Manual for more details.
+
+mpu401=intelligent
+mididevice=default
+midiconfig=
+
+[sblaster]
+#  sbtype: Type of Soundblaster to emulate. gb is Gameblaster.
+#          Possible values: sb1, sb2, sbpro1, sbpro2, sb16, gb, none.
+#  sbbase: The IO address of the soundblaster.
+#          Possible values: 220, 240, 260, 280, 2a0, 2c0, 2e0, 300.
+#     irq: The IRQ number of the soundblaster.
+#          Possible values: 7, 5, 3, 9, 10, 11, 12.
+#     dma: The DMA number of the soundblaster.
+#          Possible values: 1, 5, 0, 3, 6, 7.
+#    hdma: The High DMA number of the soundblaster.
+#          Possible values: 1, 5, 0, 3, 6, 7.
+# sbmixer: Allow the soundblaster mixer to modify the DOSBox mixer.
+# oplmode: Type of OPL emulation. On 'auto' the mode is determined by sblaster type. All OPL modes are Adlib-compatible, except for 'cms'.
+#          Possible values: auto, cms, opl2, dualopl2, opl3, none.
+#  oplemu: Provider for the OPL emulation. compat might provide better quality (see oplrate as well).
+#          Possible values: default, compat, fast.
+# oplrate: Sample rate of OPL music emulation. Use 49716 for highest quality (set the mixer rate accordingly).
+#          Possible values: 44100, 49716, 48000, 32000, 22050, 16000, 11025, 8000.
+
+sbtype=sb16
+sbbase=220
+irq=7
+dma=1
+hdma=5
+sbmixer=true
+oplmode=auto
+oplemu=default
+oplrate=44100
+
+[gus]
+#      gus: Enable the Gravis Ultrasound emulation.
+#  gusrate: Sample rate of Ultrasound emulation.
+#           Possible values: 44100, 48000, 32000, 22050, 16000, 11025, 8000, 49716.
+#  gusbase: The IO base address of the Gravis Ultrasound.
+#           Possible values: 240, 220, 260, 280, 2a0, 2c0, 2e0, 300.
+#   gusirq: The IRQ number of the Gravis Ultrasound.
+#           Possible values: 5, 3, 7, 9, 10, 11, 12.
+#   gusdma: The DMA channel of the Gravis Ultrasound.
+#           Possible values: 3, 0, 1, 5, 6, 7.
+# ultradir: Path to Ultrasound directory. In this directory
+#           there should be a MIDI directory that contains
+#           the patch files for GUS playback. Patch sets used
+#           with Timidity should work fine.
+
+gus=false
+gusrate=44100
+gusbase=240
+gusirq=5
+gusdma=3
+ultradir=C:ULTRASND
+
+[speaker]
+# pcspeaker: Enable PC-Speaker emulation.
+#    pcrate: Sample rate of the PC-Speaker sound generation.
+#            Possible values: 44100, 48000, 32000, 22050, 16000, 11025, 8000, 49716.
+#     tandy: Enable Tandy Sound System emulation. For 'auto', emulation is present only if machine is set to 'tandy'.
+#            Possible values: auto, on, off.
+# tandyrate: Sample rate of the Tandy 3-Voice generation.
+#            Possible values: 44100, 48000, 32000, 22050, 16000, 11025, 8000, 49716.
+#    disney: Enable Disney Sound Source emulation. (Covox Voice Master and Speech Thing compatible).
+
+pcspeaker=true
+pcrate=44100
+tandy=auto
+tandyrate=44100
+disney=true
+
+[joystick]
+# joysticktype: Type of joystick to emulate: auto (default), none,
+#               2axis (supports two joysticks),
+#               4axis (supports one joystick, first joystick used),
+#               4axis_2 (supports one joystick, second joystick used),
+#               fcs (Thrustmaster), ch (CH Flightstick).
+#               none disables joystick emulation.
+#               auto chooses emulation depending on real joystick(s).
+#               (Remember to reset dosbox's mapperfile if you saved it earlier)
+#               Possible values: auto, 2axis, 4axis, 4axis_2, fcs, ch, none.
+#        timed: enable timed intervals for axis. Experiment with this option, if your joystick drifts (away).
+#     autofire: continuously fires as long as you keep the button pressed.
+#       swap34: swap the 3rd and the 4th axis. can be useful for certain joysticks.
+#   buttonwrap: enable button wrapping at the number of emulated buttons.
+
+joysticktype=auto
+timed=true
+autofire=false
+swap34=false
+buttonwrap=false
+
+[serial]
+# serial1: set type of device connected to com port.
+#          Can be disabled, dummy, modem, nullmodem, directserial.
+#          Additional parameters must be in the same line in the form of
+#          parameter:value. Parameter for all types is irq (optional).
+#          for directserial: realport (required), rxdelay (optional).
+#                           (realport:COM1 realport:ttyS0).
+#          for modem: listenport (optional).
+#          for nullmodem: server, rxdelay, txdelay, telnet, usedtr,
+#                         transparent, port, inhsocket (all optional).
+#          Example: serial1=modem listenport:5000
+#          Possible values: dummy, disabled, modem, nullmodem, directserial.
+# serial2: see serial1
+#          Possible values: dummy, disabled, modem, nullmodem, directserial.
+# serial3: see serial1
+#          Possible values: dummy, disabled, modem, nullmodem, directserial.
+# serial4: see serial1
+#          Possible values: dummy, disabled, modem, nullmodem, directserial.
+
+serial1=nullmodem server port:5001
+serial2=dummy
+serial3=disabled
+serial4=disabled
+
+[dos]
+#            xms: Enable XMS support.
+#            ems: Enable EMS support.
+#            umb: Enable UMB support.
+# keyboardlayout: Language code of the keyboard layout (or none).
+
+xms=true
+ems=false
+umb=false
+keyboardlayout=auto
 
 [ipx]
-ipx = false
+# ipx: Enable ipx over UDP/IP emulation.
 
-[ne2000]
-ne2000  = false # helps nothing 
+ipx=false
 
 [autoexec]
-imgmount c win98.img -t hdd
+imgmount c win98.img -t hdd -size 512,63,16,255
 boot -l c
 `;
 
@@ -153,7 +230,7 @@ boot -l c
     fs.createFile("dosbox-jsdos.conf", myConf);
       
       const buf = window.electronAPI.readWin98Image();
-
+      //const imgPath = path.join(process.cwd(), "win98.img");
       fs.createFile("win98.img", buf);
 
       console.log("win98.img loaded");
